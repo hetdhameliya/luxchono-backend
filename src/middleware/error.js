4ef78a9292f5e5cb5018d36e6e5a12764a089c2c
smit monpara <monparasmit1@gmail.com>
2024-01-03 22:14:56 +0530

    DONE:- status code change

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -2,8 +2,8 @@ const ApiError = require("../util/error");
 
 module.exports = function (err, req, res, next) {
   if (err instanceof ApiError) {
-    res.status(err.statusCode).json({ statusCode: err.statusCode, success: false, message: err.message });
+    res.status(200).json({ statusCode: err.statusCode, success: false, message: err.message });
   } else {
-    res.status(500).json({ statusCode: 500, success: false, message: "Internal server error" });
+    res.status(200).json({ statusCode: 500, success: false, message: "Internal server error" });
   }
 };
