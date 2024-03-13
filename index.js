e2217852942111662c0f3eb69325a18417ae052e
smit monpara <monparasmit1@gmail.com>
2024-03-13 19:31:24 +0530

    REMOVE:- file upload

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ const router = require("./src/router/router");
 const adminRouter = require("./src/router/admin_router");
 const errorMiddleware = require("./src/middleware/error");
 const databaseConnect = require("./src/database/index");
-const upload = require("./src/middleware/multer");
-const ApiError = require("./src/util/error");
-const { cloudinary } = require("./src/util/cloudinary");
-const fs = require("fs");
 
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 databaseConnect();
 
 app.get("/", (_req, res) => {
@@ -24,19 +20,6 @@ app.get("/", (_req, res) => {
 
 app.use(router);
 app.use("/admin", adminRouter);
-app.post("/file-upload", upload.single("file"), async (req, res, next) => {
-  try {
-    const file = req.file;
-    if (file) {
-      const result = await cloudinary.uploader.upload(file.path, { use_filename: true });
-      fs.unlinkSync(file.path);
-      return res.status(200).json({ statusCode: 200, success: true, data: result });
-    }
-    return next(new ApiError(400, "Please insert file"));
-  } catch (e) {
-    return next(new ApiError(400, e.message));
-  }
-});
 app.use(errorMiddleware);
 
 
