a5b349e89a22ccbe378a134498a4c3ebbe935208
smit monpara <monparasmit1@gmail.com>
2024-03-16 13:01:28 +0530

    DONE:- rating message change

--- src/controller/rating_controller.js
+++ src/controller/rating_controller.js
@@ -1,11 +1,17 @@
 const ApiError = require("../util/error");
 const RatingModel = require("../model/rating_model");
 const OrderModel = require("../model/order_model");
+const { DELIVERED_STATUS } = require("../config/string");
+const { isValidObjectId } = require("mongoose");
 
 async function addRating(req, res, next) {
     try {
         const { product, description, star } = req.body;
         const id = req.id;
+        const isValid = isValidObjectId(product);
+        if (!isValid) {
+            return next(new ApiError(400, "Id is invalid"));
+        }
         const foundRating = await RatingModel.findOne({ user: id, product });
         if (foundRating) {
             foundRating.description = description;
@@ -13,7 +19,7 @@ async function addRating(req, res, next) {
             await foundRating.save();
             return res.status(200).json({ statusCode: 200, success: true, message: 'Thank you for your rating! Your feedback helps us improve our products and services. We appreciate your valuable input.' });
         }
-        const foundOrder = await OrderModel.findOne({ user: id, "products.product": product });
+        const foundOrder = await OrderModel.findOne({ user: id, "products.product": product, status: DELIVERED_STATUS });
         if (!foundOrder) {
             return next(new ApiError(400, "You can not give rating for this product"));
         }
@@ -21,7 +27,7 @@ async function addRating(req, res, next) {
         await rating.save();
         res.status(200).json({ statusCode: 200, success: true, message: 'Thank you for your rating! Your feedback helps us improve our products and services. We appreciate your valuable input.' });
     } catch (e) {
-        return next(new ApiError(400, 'Enter valid address details'));
+        return next(new ApiError(400, 'Please try again..'));
     }
 }
 
